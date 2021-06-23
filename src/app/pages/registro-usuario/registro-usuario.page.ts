eb15bdafeeb8c6bfc0b42d2befe67c0f707d368a
Christian Aguayo <christian.aguayo.pea@cftmail.cl>
2021-06-22 21:41:59 -0400

    .)

--- src/app/pages/registro-usuario/registro-usuario.page.ts
+++ src/app/pages/registro-usuario/registro-usuario.page.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class EnviarDatos {
+ constructor(
+   public dato1,
+   public dato2,
+   public dato3,
+   public dato4,
+   public dato5,
+ ){}
+
+}
+
 @Component({
   selector: 'app-registro-usuario',
   templateUrl: './registro-usuario.page.html',
@@ -7,9 +18,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroUsuarioPage implements OnInit {
 
-  constructor() { }
+  datos_formulario;
+  constructor() { 
+    this.datos_formulario = new EnviarDatos("Ingrese su nombre", "Ingrese su apellido" , "0", "Ingrese su email", "contraseña");
+  }
 
   ngOnInit() {
   }
 
+  enviarDatos(){
+    console.log("Datos del formulario: ", this.datos_formulario)
+  }
 }
